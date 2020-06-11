8e28842f8462e1bfb53c5847642422b9c10c8e56
Pikachuxxxx <phani.s2909@icloud.com>
2020-06-11 17:29:10 +0530

    Added formward root motion movement

---
+++ Third Person Humanoid Character/Assets/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Animator))]
+public class Movement : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //move
+        Move();
+        //turn
+
+        //jump
+    }
+
+    private void Turn()
+    {
+
+    }
+    private void Move()
+    {
+        anim.SetFloat(ACParams.MoveX, Input.GetAxis(Axis.Horizontal));
+        anim.SetFloat(ACParams.MoveZ, Input.GetAxis(Axis.Vertical));
+    }
+}
---
+++ Third Person Humanoid Character/Assets/TagHolder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ACParams
+{
+    public const string MoveX = "MoveX";
+    public const string MoveZ = "MoveZ";
+    public const string Turn = "Turn";
+    public const string Jump = "Jump";
+}
+
+public class Axis
+{
+    public const string Horizontal = "Horizontal";
+    public const string Vertical = "Vertical";
+}
